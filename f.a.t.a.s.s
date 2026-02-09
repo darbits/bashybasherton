9c5a69185728a49bff504001d75a452b477ea0c6
darbits <78238130+darbits@users.noreply.github.com>
2026-02-09 20:52:54 +0000

    Changes to be committed: 	new file:   .f.a.t.a.s.s.swp 	modified:   f.a.t.a.s.s 	modified:   foodlist.txt 	new file:   workout.txt lololol i'm afraid of commitments

--- f.a.t.a.s.s
+++ f.a.t.a.s.s
@@ -1,16 +1,53 @@
 #!/usr/bin/env bash
+
+
 #get name of food eaten, test if already in database, append if needed
 	
 food(){
-	read -p "what did you eat? : " food 
-	local food="$food"
-if grep -q "$food*" foodlist.txt ; then
-	echo ""$food"? Great."
+	read -p "enter new food record : " food 
+	read -p "use singular tense and input unit of measurement for new food : " unit 
+	read -p "how many calories does one 'unit' of this food contain? : " calin 
 	
-else 
-	echo "$food" >> foodlist.txt
+	local $food
+	local $unit
+	local $calin
 
-fi
+if grep -qw "$food*"  foodlist.txt ; then
+	read -p "an entry for "$food*" already exists, do you want to replace it? 'y' will replace 'n' will add a new entry - hit enter to exit : " yn  
+		case $yn in
+			
+			"")
+				 exit;;
+			y)
+				echo "boobies";;
+			n)						
+				echo ""$food" "$unit" "$calin"" >> foodlist.txt
+				echo "new entry for "$food" added
+				exit;;
+		esac
+else	
+	echo "new entry added"
+	echo ""$food" "$unit" "$calin"" >> foodlist.txt
+				exit		
+fi	
 }
+
+#beginning step for activity
+#
+#activity(){
+#	read -p "what activity did we do? Enter if none : " activity 3	local activity="$activity"
+#if grep -q "$activity*" workout.txt ; then
+#	echo ""$activity"? Great."
+#
+#elif	[[ -z "$activity" ]] ; then
+#	exit 
+#	
+#else 
+#	echo "$activity" >> workout.txt
+#
+#fi
+#}
+
+#activity
 
 food
